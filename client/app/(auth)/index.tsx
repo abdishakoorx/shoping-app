c51e696e45c7b7af03150d8185119728da85f6b5
Hassan Abdishakoor <hassanabdishakoor@gmail.com>
2025-01-21 14:13:03 +0300

    add clerk auth

---
+++ client/app/(auth)/index.tsx
@@ -0,0 +1,11 @@
+import { ThemedText } from "@/components/ThemedText";
+import { Text, View } from "react-native";
+
+export default function SingInScreen() {
+  return (
+    <View>
+      <ThemedText type="title">Wassup</ThemedText>
+      <ThemedText type="default">Signin</ThemedText>
+    </View>
+  );
+}
